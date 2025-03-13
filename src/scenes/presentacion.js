2c5966b00a461b3d4ead653e8c41d83396c3acad
Hugo Vazquez Hernandez <hernandezhugo448@gmail.com>
2025-03-13 13:27:15 -0600

    Pantallas
    Agregue las pantallas de cada escena en la carpeta "scenes"

---
+++ src/scenes/presentacion.js
@@ -0,0 +1,21 @@
+export default class Presentacion extends Phaser.Scene {
+    constructor() {
+        super({ key: 'Presentacion' });
+    }
+
+    preload() {
+        this.load.image('logo', '../assets/logo.png');
+    }
+
+    create() {
+        this.add.image(400, 300, 'logo');
+        this.add.text(400, 500, 'Nombre del Juego', {
+            fontSize: '48px',
+            fill: '#fff'
+        }).setOrigin(0.5);
+
+        this.time.delayedCall(3000, () => {
+            this.scene.start('MenuPrincipal');
+        });
+    }
+}
